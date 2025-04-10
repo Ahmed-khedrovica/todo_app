5b85c75fe22d1657f079aeeded78d81d6a627b1b
Your Name <you@example.com>
2025-04-10 19:47:04 +0200

    create edit note view

--- lib/widgets/custom_note_item.dart
+++ lib/widgets/custom_note_item.dart
@@ -1,56 +1,67 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:todo_app/views/edit_note_view.dart';
 
 class NoteItem extends StatelessWidget {
   const NoteItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.only(top: 24,bottom: 24,left: 16),
-      decoration: BoxDecoration(
-        color: Colors.blue,
-        borderRadius: BorderRadius.circular(16),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          ListTile(
-            title: const Text(
-              'Flutter Tips',
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 25,
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) {
+            return const EditNoteView();
+          }),
+        );
+      },
+      child: Container(
+        padding: EdgeInsets.only(top: 24, bottom: 24, left: 16),
+        decoration: BoxDecoration(
+          color: Colors.blue,
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            ListTile(
+              title: const Text(
+                'Flutter Tips',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 25,
+                ),
+              ),
+              subtitle: Padding(
+                padding: const EdgeInsets.only(top: 16),
+                child: Text(
+                  'Build Your Career',
+                  style: TextStyle(
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+              trailing: IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  FontAwesomeIcons.trash,
+                  color: Colors.black,
+                  size: 20,
+                ),
               ),
             ),
-            subtitle: Padding(
-              padding: const EdgeInsets.only(top: 16),
+            Padding(
+              padding: const EdgeInsets.only(right: 24),
               child: Text(
-                'Build Your Career',
+                'May 21, 2022',
                 style: TextStyle(
                   color: Colors.black,
                 ),
               ),
             ),
-            trailing: IconButton(
-              onPressed: () {},
-              icon: Icon(
-                FontAwesomeIcons.trash,
-                color: Colors.black,
-                size: 20,
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(right: 24),
-            child: Text(
-              'May 21, 2022',
-              style: TextStyle(
-                color: Colors.black,
-              ),
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
