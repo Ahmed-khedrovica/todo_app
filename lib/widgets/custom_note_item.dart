dd903e31a12ec588666fb66d9932773e937e69cb
Your Name <you@example.com>
2025-04-23 00:36:00 +0200

    display data & format date time

--- lib/widgets/custom_note_item.dart
+++ lib/widgets/custom_note_item.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:todo_app/models/note_model.dart';
 import 'package:todo_app/views/edit_note_view.dart';
 
 class NoteItem extends StatelessWidget {
-  const NoteItem({super.key});
+  const NoteItem({super.key, required this.note});
 
+  final NoteModel note;
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
@@ -19,24 +21,24 @@ class NoteItem extends StatelessWidget {
       child: Container(
         padding: EdgeInsets.only(top: 24, bottom: 24, left: 16),
         decoration: BoxDecoration(
-          color: Colors.blue,
+          color: Color(note.color),
           borderRadius: BorderRadius.circular(16),
         ),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.end,
           children: [
             ListTile(
-              title: const Text(
-                'Flutter Tips',
-                style: TextStyle(
+              title:  Text(
+                note.title,
+                style: const TextStyle(
                   color: Colors.black,
                   fontSize: 25,
                 ),
               ),
               subtitle: Padding(
-                padding: const EdgeInsets.only(top: 16),
+                padding:  EdgeInsets.only(top: 16),
                 child: Text(
-                  'Build Your Career',
+                  note.subTitle,
                   style: TextStyle(
                     color: Colors.black,
                   ),
@@ -54,7 +56,7 @@ class NoteItem extends StatelessWidget {
             Padding(
               padding: const EdgeInsets.only(right: 24),
               child: Text(
-                'May 21, 2022',
+                note.date,
                 style: TextStyle(
                   color: Colors.black,
                 ),
